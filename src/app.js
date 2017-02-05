31c0029796abccec95c6062cc4c4178d095562f1
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2017-02-05 02:25:39 -0200

    Criação do componente principal do projeto: 'QuizzApp' e alterações de css e html na página principal

--- src/app.js
+++ src/app.js
@@ -1,14 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import MeuComponente from './components/MeuComponente';
+import QuizzApp from './components/QuizzApp';
 
-export default class HelloWorld extends React.Component {
-	render() {
-		return (
-			<h1>Fala Glaucia! Beleza?!</h1>
-		);
-	}
-}
-
-ReactDOM.render(<MeuComponente/>, document.getElementById('app'));
+ReactDOM.render(<QuizzApp />, document.getElementById("app"));
---
+++ src/components/QuizzApp.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class QuizzApp extends React.Component {
+	render() {
+		return (
+			<div className="jumbotron">
+				<h1 className="text-center"><span className="glyphicon fa fa-futbol-o" aria-hidden="true"></span>Quizz Futebol!!</h1>
+    		</div>
+		);
+	}
+};
