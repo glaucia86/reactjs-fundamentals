d917b7a54ee8912ba520e69e3dd7e0430d52b18d
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2017-02-06 00:07:15 -0200

    Criação de um Componente Container (Principal) para a app.

--- src/components/QuizzApp.js
+++ src/components/QuizzApp.js
@@ -1,11 +1,21 @@
 import React from 'react';
 
+import QuizzContainer from './QuizzContainer';
+
 export default class QuizzApp extends React.Component {
 	render() {
 		return (
-			<div className="jumbotron">
-				<h1 className="text-center"><span className="glyphicon fa fa-futbol-o" aria-hidden="true"></span>Quizz Futebol!!</h1>
-    		</div>
+			<div>
+				<div className="jumbotron">
+					<h1 className="text-center"><span className="glyphicon fa fa-futbol-o" aria-hidden="true"></span>Quizz Futebol!!</h1>
+					<h2 className="text-center">Teste Aqui Todos os Seus Conhecimentos do Mundo do Futebol!</h2>
+    			</div>
+    			<div>
+    				<QuizzContainer />
+    			</div>
+			</div>
 		);
 	}
 };
+
+
---
+++ src/components/QuizzContainer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class QuizzContainer extends React.Component {
+	render() {
+		return (
+			<div>
+				<h4>Testando o Componente Principal da App!</h4>
+			</div>
+		);
+	}
+};
