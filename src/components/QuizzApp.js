18f23da37d4488c81764b189221af4bcdb7351a2
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2017-02-06 00:22:41 -0200

    Tratamento de Responsividade da Página.'

--- src/components/QuizzApp.js
+++ src/components/QuizzApp.js
@@ -8,7 +8,7 @@ export default class QuizzApp extends React.Component {
 			<div>
 				<div className="jumbotron">
 					<h1 className="text-center"><span className="glyphicon fa fa-futbol-o" aria-hidden="true"></span>Quizz Futebol!!</h1>
-					<h2 className="text-center">Teste Aqui Todos os Seus Conhecimentos do Mundo do Futebol!</h2>
+					<h3 className="text-center">Teste Aqui Todos os Seus Conhecimentos do Mundo do Futebol!</h3>
     			</div>
     			<div>
     				<QuizzContainer />
